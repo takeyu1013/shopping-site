fcacc95fcfbe82cda4b92a3b982b0ecea8022cf5
takeyu1013 <yuto1013@gmail.com>
2021-07-23 13:21:52 +0000

    Link Microposts to Users

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
     @micropost = microposts(:one)
   end
 
@@ -17,7 +18,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create micropost" do
     assert_difference('Micropost.count') do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @user.id } }
     end
 
     assert_redirected_to micropost_url(Micropost.last)
@@ -34,7 +35,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update micropost" do
-    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @user.id } }
     assert_redirected_to micropost_url(@micropost)
   end
 
