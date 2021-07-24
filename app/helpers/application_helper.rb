27d0d07ee78be878074d9cd61a1eabb8f2ee460e
takeyu1013 <yuto1013@gmail.com>
2021-07-24 13:28:15 +0000

    Add a function to set a default title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a full title by page.
+  def full_title(page_title = '')
+    base_title = "ショッピングサイト"
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
 end
