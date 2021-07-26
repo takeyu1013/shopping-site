59cc58ed7dc068bb4680c900e47cd7dea7f012a0
takeyu1013 <yuto1013@gmail.com>
2021-07-26 12:07:48 +0000

    Add validations of User data

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
 end
