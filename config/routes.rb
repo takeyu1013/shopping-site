75a36dfdb990f55c717bd000255ede7389cffd9f
takeyu1013 <yuto1013@gmail.com>
2021-07-26 14:24:07 +0000

    Add Session for login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/login', to: 'sessions#destroy'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
