1d8b4d6594687ccb3345c2e17d579405936604c7
takeyu1013 <yuto1013@gmail.com>
2021-07-28 13:46:29 +0000

    Add test data for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
