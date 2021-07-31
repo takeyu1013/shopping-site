21d7ad3d37cc7513fd77a67b05f41c86ee9eae21
takeyu1013 <yuto1013@gmail.com>
2021-07-31 02:26:48 +0000

    Change a redirect route after login to item list

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to controller: :items, action: :index
     else
       # TODO
       render 'new'
