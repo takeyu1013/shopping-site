68064ea822006c0dea28b9a1a7c3548f94515987
takeyu1013 <yuto1013@gmail.com>
2021-08-01 03:22:06 +0000

    Fix a signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
