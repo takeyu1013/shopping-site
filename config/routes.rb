b27b06b74b93993f5d232e6ef1f35a3e8a0a2b57
takeyu1013 <yuto1013@gmail.com>
2021-08-05 14:21:01 +0000

    Refactor a routing path

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  post 'items/show', to: 'cart_items#create'
+  post 'items/:id', to: 'cart_items#create'
   get 'cart_items/index'
   post 'cart_items/index', to: 'orders#create'
   resources :microposts
