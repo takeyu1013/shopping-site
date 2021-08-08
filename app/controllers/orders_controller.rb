128cd12510549b12a4a47b2256a26a10b59f3925
takeyu1013 <yuto1013@gmail.com>
2021-08-08 09:13:44 +0000

    Add a feature to update item stocks after ordered

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,11 @@ class OrdersController < ApplicationController
     @total_price = params[:total_price].to_i
     @order = current_user.orders.new(price: @total_price)
     @order.save
-    @cart_items = current_user.cart_items
+    @cart_items = current_user.cart_items.where(order_id: nil)
     @cart_items.each do | cart_item |
       cart_item.order_id = @order.id
+      cart_item.item.stock -= cart_item.quantity
+      cart_item.item.save
       cart_item.save
     end
     redirect_to orders_index_path
